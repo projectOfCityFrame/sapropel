d9df8422dadfe599886ec459d982578110f48824
Nikita <nikitaelkin7@gmail.com>
2015-05-04 18:34:39 +0400

    contact_me

--- contact_me.php
+++ contact_me.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 	
 // Create the email and send the message
-$to = 'nikitaelkin7@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'maksim_ivanov_1968@bk.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Новая заявка:  $name";
 $email_body = "Вы получили новую заявку с вашего веб-сайта.\n\n"."Подробности:\n\nИмя: $name\n\nEmail: $email_address\n\nТелефон: $phone";
-$headers = "From: valeri43.ru\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: valeryprod.ru\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
